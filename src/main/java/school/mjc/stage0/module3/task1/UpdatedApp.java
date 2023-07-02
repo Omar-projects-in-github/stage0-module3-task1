ed9519ebb67496192e097cec4ad52786aac8ccba
Omar-projects-in-github <polat.omar2002@gmail.com>
2023-07-03 00:20:03 +0600

    UpdatedApp class changed place logger and system

--- src/main/java/school/mjc/stage0/module3/task1/UpdatedApp.java
+++ src/main/java/school/mjc/stage0/module3/task1/UpdatedApp.java
@@ -5,9 +5,9 @@ import java.util.logging.Logger;
 public class UpdatedApp {
     private static final Logger logger = Logger.getLogger(UpdatedApp.class.getName());
     public static void main(String[] args) {
-        logger.info("I'm updated," +
-                "\ncan be compiled. And can write to console");
         System.out.println("I'm updated," +
+                "\ncan be compiled. And can write to console");
+        logger.info("I'm updated," +
                 "\ncan be compiled. And can write to console");
     }
 }
