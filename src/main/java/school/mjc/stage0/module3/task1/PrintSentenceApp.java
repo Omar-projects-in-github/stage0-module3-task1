d183485e0b14f2ab1d691bbb98eb22b101985de9
Omar-projects-in-github <polat.omar2002@gmail.com>
2023-07-03 00:49:54 +0600

    making all changes as previous

--- src/main/java/school/mjc/stage0/module3/task1/PrintSentenceApp.java
+++ src/main/java/school/mjc/stage0/module3/task1/PrintSentenceApp.java
@@ -3,9 +3,7 @@ package school.mjc.stage0.module3.task1;
 import java.util.logging.Logger;
 
 public class PrintSentenceApp {
-    private static final Logger logger = Logger.getLogger(PrintSentenceApp.class.getName());
     public static void main(String[] args) {
-        logger.info("This is my first Java program");
         System.out.println("This is my first Java program");
     }
 }
