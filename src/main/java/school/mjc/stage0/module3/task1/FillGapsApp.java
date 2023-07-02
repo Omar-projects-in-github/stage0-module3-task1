3360c93fcb864714af6f83c8c6792210756aaf4e
Omar-projects-in-github <polat.omar2002@gmail.com>
2023-07-03 00:39:56 +0600

    Revert "I have changed some code to make my code quality better"
    This reverts commit b41b3c62

--- src/main/java/school/mjc/stage0/module3/task1/FillGapsApp.java
+++ src/main/java/school/mjc/stage0/module3/task1/FillGapsApp.java
@@ -1,11 +1,7 @@
 package school.mjc.stage0.module3.task1;
 
-import java.util.logging.Logger;
-
 public class FillGapsApp {
-    private static final Logger logger = Logger.getLogger(FillGapsApp.class.getName());
     public static void main(String[] args) {
-        logger.info("Fixed");
         System.out.println("Fixed");
     }
 }
